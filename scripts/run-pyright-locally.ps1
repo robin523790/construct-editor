f4d201f1506ac6d375a469ee674e07bf58471721
Robin.Schmidt <robin.schmidt@winkhaus.de>
2026-06-09 10:14:17 +0200

    Removed unused imports

--- scripts/run-pyright-locally.ps1
+++ scripts/run-pyright-locally.ps1
@@ -1,10 +1,9 @@
-# Simpler wrapper, da PyCharm sich etwas anstellt, wenn man kein Skript hat.
-# PowerShell, damit es auch unter Linux funktionieren würde.
+# Simple wrapper script to run all code checks, from fastest to slowest.
 $ErrorActionPreference = "Stop"
 ./.venv/Scripts/activate.ps1
 
-Write-Host("Running ruff check --show-fixes ...")
-ruff check --show-fixes
+Write-Host("Running ruff check --fix --show-fixes ...")
+ruff check --fix --show-fixes
 if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
@@ -18,5 +17,5 @@ if ($LASTEXITCODE -ne 0) {
 
 Write-Host("")
 Write-Host("Running PyRight...")
-npm exec --yes '--' pyright@1.1.408 @args
+npm exec --yes --package=pyright@1.1.408 -- pyright @args
 exit $LASTEXITCODE
